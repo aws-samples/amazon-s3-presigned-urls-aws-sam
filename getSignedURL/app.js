5d6c35e9fff3feba94f35d33fc8a321678cc4d00
AyodeleAwuna <69871742+AyodeleAwuna@users.noreply.github.com>
2021-08-19 12:19:54 -0400

    Update app.js

--- getSignedURL/app.js
+++ getSignedURL/app.js
@@ -36,7 +36,7 @@ const getUploadURL = async function(event) {
     Bucket: process.env.UploadBucket,
     Key,
     Expires: URL_EXPIRATION_SECONDS,
-    ContentType: 'image/jpeg',
+    ContentType: 'video/mp4',
 
     // This ACL makes the uploaded object publicly readable. You must also uncomment
     // the extra permission for the Lambda function in the SAM template.
